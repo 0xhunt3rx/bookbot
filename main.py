7f1627f1d4144cc7a40c6cd440353acfa4721b7e
0xhunt3rx <0xhunt3rx@gmail.com>
2025-04-14 12:51:58 +0100

    Add get_num_chars() function and test it

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, get_num_chars
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -7,7 +7,11 @@ def get_book_text(filepath):
 
 def main():
     text = get_book_text("books/frankenstein.txt")
+    chars = get_num_chars(text)
     length = len(get_num_words(text))
+
     print(f"{length} words found in the document")
+    for c in chars:
+        print(f"'{c}': {chars[c]}")
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,11 @@
 def get_num_words(text):
     return text.split()
+
+def get_num_chars(text):
+    chars = {}
+    for c in text:
+        c = c.lower()
+        if c not in chars:
+            chars[c] = 0
+        chars[c] += 1
+    return chars
