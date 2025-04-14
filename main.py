5492b912f3fd891e638f1292014b8caa851a8d61
0xhunt3rx <0xhunt3rx@gmail.com>
2025-04-14 12:10:36 +0100

    Add to_words() function

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+def to_words(text):
+    return text.split()
+
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
         return file_contents
 
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    text = get_book_text("books/frankenstein.txt")
+    length = len(to_words(text))
+    print(f"{length} words found in the document")
 
 main()
