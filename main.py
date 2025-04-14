84c7c9c0576d4d0b0e9ff1813f5c2739736d8f81
0xhunt3rx <0xhunt3rx@gmail.com>
2025-04-14 14:39:22 +0100

    Refactor to analyse the books in 2nd argv

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 from stats import *
-
-def get_book_text(filepath):
-    with open(filepath) as f:
-        file_contents = f.read()
-        return file_contents
+import sys
 
 def main():
-    filepath = "books/frankenstein.txt"
-    text = get_book_text(filepath)
+
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    text = get_book_text(sys.argv[1])
     chars = get_num_chars(text)
     sorted_chars = get_sorted_chars(chars)
 
     length = len(get_num_words(text))
 
+
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {filepath}...")
+    print(f"Analyzing book found at {sys.argv[1]}...")
     print("----------- Word Count ----------")
     print(f"Found {length} total words")
     print("--------- Character Count -------")
@@ -22,5 +23,10 @@ def main():
         if c["name"].isalpha():
             print(f"{c["name"]}: {c["num"]}")
     print("============= END ===============")
+
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+        return file_contents
 
 main()
